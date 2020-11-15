f65a2de5aa6d4b9e6feeccc65e0eea6d3296ce67
Daniel Sanchez <jdanielsancheza@gmail.com>
2020-11-15 01:12:53 -0500

    States y Pathfinding

---
+++ Assets/Scripts/IA Pathfinding/IAStates.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pathfinding;
+
+public class IAStates : AIPath
+{
+    
+    Rigidbody2D rb;
+    AIDestinationSetter destinationSetter;
+    Transform player;
+
+    bool destinationReached;
+
+    //Idle es caminar por ahí, no estar quieto
+    public enum IAStatus { Idle, Chasing, Investigating}
+    public IAStatus state = new IAStatus();
+
+
+    // Start is called before the first frame update
+    new void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        seeker = GetComponent<Seeker>();
+        rb = GetComponent<Rigidbody2D>();
+        destinationSetter = GetComponent<AIDestinationSetter>();
+    }
+
+    public override void OnTargetReached()
+    {
+        if (!destinationReached)
+        {
+            destinationReached = true;
+        }
+        else
+        {
+            //acción deseada
+            //timer
+            StartCoroutine(Cooldown(2));
+        }
+    }
+
+    public void GoToEmergency(Transform place)
+    {
+        state = IAStatus.Investigating;
+        destinationSetter.target = place;
+    }
+
+    public void ChasePJ()
+    {
+        state = IAStatus.Chasing;
+        destinationSetter.target = player;
+    }
+   
+    IEnumerator Cooldown(int secs)
+    {
+        yield return new WaitForSeconds(secs);
+    }
+
+    //private void FixedUpdate()
+    //{
+    //    if (path == null) return;
+
+    //    if(currentWaypoint >= path.vectorPath.Count)
+    //    {
+    //        reachedEndOfPath = true;
+    //        return;
+    //    }
+    //    else
+    //    {
+    //        reachedEndOfPath = false;
+    //    }
+
+    //    float distance = Vector2.Distance(rb.position, path.vectorPath[currentWaypoint]);
+
+    //    if (distance < nextWaypointDistance)
+    //    {
+    //        currentWaypoint++;
+    //    }
+    //}
+
+}
