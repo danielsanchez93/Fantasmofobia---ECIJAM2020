8f8e3a8a31084d7f1b361e5afeca697d21f7f20d
Pochirojas <srojasbon@gmail.com>
2020-11-15 18:57:58 -0500

    Finished Player character

--- Assets/Scripts/PlayerScripts/PlayerAbility.cs
+++ Assets/Scripts/PlayerScripts/PlayerAbility.cs
@@ -7,9 +7,19 @@ public class PlayerAbility : MonoBehaviour
     // This is the class for the players ability.
     // the player will be able to phase through walls 
     //after the use of the ability there will be a cooldown 
+    #region declarations
 
-    GameObject[] walls;
-    public int CooldownTimer;
+    private GameObject[] walls;
+    public int cooldownTimer;
+    public bool IsInCooldown;
+    //public bool IsNearMark;
+    //private float startTime = 0f;
+    //private float Timer = 0f;
+    //public float holdTime = 3f;
+    //private bool held = false;
+
+    #endregion
+
     private void Awake()
     {
         walls = GameObject.FindGameObjectsWithTag("WallInterior");
@@ -19,21 +29,40 @@ public class PlayerAbility : MonoBehaviour
     {
         if (collision.CompareTag("WallInterior"))
         {
-            foreach(GameObject wall in walls)
+            foreach (GameObject wall in walls)
             {
                 wall.GetComponent<BoxCollider2D>().isTrigger = false;
+                wall.GetComponent<SpriteRenderer>().color = new Color32(120,120,120,120);
             }
-            GameObject.FindGameObjectsWithTag("WallInterior")[0].GetComponent<BoxCollider2D>().isTrigger = false;
+            //GameObject.FindGameObjectsWithTag("WallInterior")[0].GetComponent<BoxCollider2D>().isTrigger = false;
+            print("Istrigger = false");
             StartCoroutine("timer");
         }
     }
 
+    public void LightCooldown()
+    {
+        StartCoroutine("Cooldown");
+    }
+
+
     IEnumerator timer()
     {
-        yield return new WaitForSeconds(CooldownTimer);
+        yield return new WaitForSeconds(cooldownTimer);
         foreach (GameObject wall in walls)
         {
             wall.GetComponent<BoxCollider2D>().isTrigger = true;
+            print("Istrigger = true");
+            wall.GetComponent<SpriteRenderer>().color = Color.black;
         }
     }
+
+    IEnumerator Cooldown()
+    {
+        IsInCooldown = true;
+        yield return new WaitForSeconds(cooldownTimer);
+        IsInCooldown = false;
+    }
 }
+
+
